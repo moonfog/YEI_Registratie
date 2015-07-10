025725dca8f5ae422141a895c06ed95cc4c98244
tom natus <tombo@macmini.local>
2015-07-10 11:34:11 +0200

    working on models added guest

--- models/db.py
+++ models/db.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import datetime
 
 #########################################################################
 ## This scaffolding model makes your app work on Google App Engine too
@@ -58,7 +59,11 @@ service = Service()
 plugins = PluginManager()
 
 ## create all tables needed by auth if not custom tables
-auth.define_tables(username=False, signature=False)
+auth.settings.table_user_name = 'user'
+auth.settings.table_group_name = 'group_role'
+auth.settings.table_membership_name = 'group_membership'
+auth.settings.table_permission_name = 'permission'
+auth.define_tables()
 
 ## configure email
 mail = auth.settings.mailer
@@ -67,6 +72,8 @@ mail.settings.sender = myconf.take('smtp.sender')
 mail.settings.login = myconf.take('smtp.login')
 
 ## configure auth policy
+auth.settings.actions_disabled.append('register')
+auth.settings.login_url = URL('user', args = 'login')
 auth.settings.registration_requires_verification = False
 auth.settings.registration_requires_approval = False
 auth.settings.reset_password_requires_verification = True
@@ -88,5 +95,100 @@ auth.settings.reset_password_requires_verification = True
 ## >>> for row in rows: print row.id, row.myfield
 #########################################################################
 
+#########################################################################
+#defining some sets i need 
+years = []
+guest_errors = None
+guest_errors = []
+#########################################################################
 ## after defining tables, uncomment below to enable auditing
-# auth.enable_record_versioning(db)
+auth.enable_record_versioning(db)
+
+
+db.define_table('category',
+	Field('category',unique=True,notnull=True,label=T('Category')),
+	format = '%(category)s')
+
+db.define_table('value',
+	Field('category',db.category,notnull=True,label=T('Category')),
+	Field('val',notnull=True,label=T('Value')),
+	Field('deleted',boolean,default=False,label=T('Deleted')))
+db.value.category.requires=IS_IN_DB(db,'category.id','%(category)s')
+
+def getCategory(value):
+	row = db(db.category.category==value).select().first()
+
+def valueNotEmpty(value):
+	if(getCategory(value)==None):
+		return False
+	else :
+		row = db(db.value.category == getCategory(value).id).select().first()
+		if row!=None:
+			return True
+		else:
+			return False
+
+def makeYears():
+	i = 0
+	year = datetime.date.today().year
+	while i < 100:
+		i = i + 1
+		years.append(year)
+		year = year - 1
+
+makeYears()
+
+db.define_table('Guest',
+	Field('firstname',notnull=True,label=T('Firstname')),
+	Field('familyname',notnull=True,label=T('Family name')),
+	Field('birth_year','integer',notnull=True,label=T('Birth year')),
+	Field('sex',db.value,notnull=True,label=T('Sex')),
+	Field('national_number',notnull=True,label=T('National number')),
+	Field('registration_date',date,default=request.now,label=T('Registration date')),
+	Field('user',db.user,notmull=True,label=T('Registered by')),
+	Field('age',compute=lambda r: (datetime.datetime.now().year) - int(r['birth_year']),label=T('Age')),
+	format = '%(name)s')
+
+db.guest.birth_year.requires=IS_IN_SET(years,zero=T('Choose One'),error_message = T('Choose a year from the list'))
+
+if(auth.user!=None):
+	guests_User = db(db.guest.user==auth.user.id)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
